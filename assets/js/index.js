2d4899d2dd521137e6ecd59361f4b51e6a563106
Ahmed El Fakharany <ahmedassem13@gmail.com>
2026-07-26 20:28:53 +0300

    dark-light feature added, scroll-navlinkactive added.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,41 @@
-// ^ Write your JavaScript code here
+var themeToggleButton = document.querySelector("#theme-toggle-button");
+var sections = document.querySelectorAll("main section");
+var navLinks = document.querySelectorAll(".nav-links a");
+var settingsToggle = document.querySelector("#settings-toggle")
+var settingsSideBar = document.querySelector("#settings-sidebar")
+
+
+
+window.addEventListener("scroll", function () {
+  var currentSection = "";
+
+  sections.forEach(function (section) {
+    if (window.scrollY >= section.offsetTop - 200) {
+      currentSection = section.id;
+    }
+  });
+
+  navLinks.forEach(function (link) {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + currentSection) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+
+settingsToggle.addEventListener("click",function(){
+    settingsSideBar.classList.toggle("translate-x-full")
+ if (settingsSideBar.classList.contains("translate-x-full")) {
+    settingsToggle.style.right = "0";
+  } else {
+    settingsToggle.style.right = "20rem";
+  }
+})
+
+themeToggleButton.addEventListener("click", function(){
+document.documentElement.classList.toggle("dark");
+});
+
